c0d8e4b5153b188af6a23b588c2c6b0dffacd4f3
Kitae's Github <aqua0405@gmail.com>
2023-06-26 16:24:19 +0900

    Update BoardRepository.java

--- src/main/java/kr/inhatc/pip/board/repository/BoardRepository.java
+++ src/main/java/kr/inhatc/pip/board/repository/BoardRepository.java
@@ -2,6 +2,12 @@ package kr.inhatc.pip.board.repository;
 
 import kr.inhatc.pip.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    // left join을 이용하여 Board와 Member를 가져옴
+    @Query("select b, w from Board b left join b.writer w where b.bno = :bno")
+    Object getBoardWithWriter(@Param("bno") Long bno);
 }
