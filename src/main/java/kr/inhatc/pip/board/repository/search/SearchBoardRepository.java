ea97a8c390c5179bf707346905ed732c1da901a3
Kitae's Github <aqua0405@gmail.com>
2023-07-07 23:03:26 +0900

    Update SearchBoardRepository.java

--- src/main/java/kr/inhatc/pip/board/repository/search/SearchBoardRepository.java
+++ src/main/java/kr/inhatc/pip/board/repository/search/SearchBoardRepository.java
@@ -1,8 +1,12 @@
 package kr.inhatc.pip.board.repository.search;
 
 import kr.inhatc.pip.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface SearchBoardRepository {
 
     public Board search1();
+
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable);
 }
